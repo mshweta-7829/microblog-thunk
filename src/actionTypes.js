eb9af1bba1026a2562902186c9f8347edafb7293
Shweta <arunbasappagudasi@Aruns-MacBook-Pro.local>
2020-10-30 14:37:15 -0700

    code to practice redux thunk

--- src/actionTypes.js
+++
@@ -1,9 +0,0 @@
-//constants to use for our app
-
-export const ADD_OR_EDIT_BLOG = "ADD_OR_EDIT_BLOG"
-export const DELETE_BLOG = "DELETE_BLOG"
-export const ADD_COMMENT = "ADD_COMMENT"
-export const DELETE_COMMENT = "DELETE_COMMENT"
-export const LOAD_TITLES = "LOAD_TITLES"
-export const GET_BLOG = "GET_BLOG"
-export const VOTE = "VOTE"
